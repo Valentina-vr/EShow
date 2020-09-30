d7a15317e0e42e70f0d430838315d2a65ed7ca7b
Valentina-vr <65366095+Valentina-vr@users.noreply.github.com>
2020-09-29 21:36:02 -0500

    Actualización de clase

---
+++ back/server.js
@@ -0,0 +1,43 @@
+let express= require('express');
+
+let app = express();
+
+app.use(express.json());
+
+app.listen(666, ()=>{
+    console.log('ESTAMOS ESCUCHANDO BIEN');
+});
+
+let usuarios = [
+    {
+        nombre: "Julanita",
+        apellidos: "Perez",
+        email: "wertfeg@gmail.com",
+        telefono: "78945613"
+    }
+]
+
+app.post('/usuarios', (req,res)=>{
+    let data=req.body;
+    console.log(data);
+
+    let email=req.body.email;
+    validarEmail(email);
+
+    usuarios.push(data);
+    console.log(usuarios);
+    res.status(200).json(data);
+})
+
+//let email = document.getElementById('email').value;
+
+
+//Validar Email
+function validarEmail(valor) {
+    if (/\w+@\w+\.+[a-z]/.test(valor)){
+        console.log("La dirección de email " + valor + " es correcta.");
+    } else {
+        console.log("La dirección de email es incorrecta.");
+    }
+}
+
